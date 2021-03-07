3a32b1530e0dbebecded9ad6415fcb8ae93cb9cb
quinnwaiwong <quinnwai.wong@gmail.com>
2021-03-07 15:29:17 -0600

    minimum files

---
+++ auth.js
@@ -0,0 +1,29 @@
+/* TODO
+Check login info using json from SQL db 
+*/
+
+//// post XMLHttpRequest request to login w/ user_auth form info ////
+// const login_info = { username: 'hi', password: 'hello' };
+// fetch('/path/to/php/file', {
+//     method: "POST",
+//     body: JSON.stringify(data)
+// })
+
+//// set up event listener to get response with either user or not user ////
+// const pathToPhpFile = 'test.php';
+// const data = { x: 'hi', y: 'hello' };
+// fetch(pathToPhpFile, {
+//     method: "POST",
+//     body: JSON.stringify(data)
+//   })
+//   .then(res => res.json())
+//   .then(response => console.log('Success:', JSON.stringify(response)))
+//   .catch(error => console.error('Error:',error))
+
+/* TODO
+if user exists, hide all, trigger event listener for calendar
+*/
+
+/* TODO
+if not, alert saying username is incorrect
+*/
---
+++ calendar.js
@@ -0,0 +1,11 @@
+/* TODO
+Add event listener for successful login, 1 variable for logged-in, 1 for show calendar (uncertain how to do that)
+also TODO: ensure there's no security issues here
+*/
+
+/*
+create calendar layout with 5x7 grid, back button, front button
+if user is logged in, events loaded into boxes (how?)
+else just the calendar no events
+*/
+
