7750297ca11e81a2dcde88699240a14f4e29048c
quinnwaiwong <quinnwai.wong@gmail.com>
2021-03-12 01:17:21 -0600

    updated fetch request w/ token; csrf validate

--- load_events.php
+++ load_events.php
@@ -1,36 +1,48 @@
 <?php
 
-/* Code for fetch request... assumes imported user variable works
+/* Code for fetch request... TODO: make sure to import user and token variable
 
 
-const data = { 'user': user };
+const data = { 'user': user, 'token': token };
 fetch('load_events.php', {
-        //Add headers
-        // Sourced from: https://stackoverflow.com/questions/37269808/react-js-uncaught-in-promise-syntaxerror-unexpected-token-in-json-at-posit
-        headers: { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify(data)
-    })
-
+    //Add headers
+    // Sourced from: https://stackoverflow.com/questions/37269808/react-js-uncaught-in-promise-syntaxerror-unexpected-token-in-json-at-posit
+    headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify(data)
+})
+.then(res => res.json())
+.then(response => {
+    if(data.success){
+        alert("event successfully added!")
+    }
+    else{
+        alert(data.message);
+    }
+});
 */
+
+header("Content-Type: application/json"); // set the MIME Type to application/json
+
+//Because you are posting the data via fetch(), php has to retrieve it elsewhere.
+$json_str = file_get_contents('php://input');
+//This will store the data into an associative array
+$json_obj = json_decode($json_str, true);
+
+//Access json object (associative array)
+$user = (string) $json_obj['user'];
+$user = (string) $json_obj['token'];
+
+// csrf vibe check
+require 'get_token.php';
 
 require 'database.php'; 
 
-// header("Content-Type: application/json"); // set the MIME Type to application/json
-
-// //Because you are posting the data via fetch(), php has to retrieve it elsewhere.
-// $json_str = file_get_contents('php://input');
-// //This will store the data into an associative array
-// $json_obj = json_decode($json_str, true);
-
-// //Access json object (associative array)
-// $user = (string) $json_obj['user'];
-
-//TODO: make sure that this works in aws first
-$user = 'firstlast';
+// debugging: making sure that this works in aws first
+// $user = 'firstlast';
 
 //TODO: require CSRF authentication
 // require '';
