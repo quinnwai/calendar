c791c6d2cc301b01e5be119a99c0869f6f5242e0
quinnwaiwong <quinnwai.wong@gmail.com>
2021-03-11 19:52:35 -0600

    authentication settled

--- guest_auth.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-$_SESSION['user'] = "";
-$_SESSION['isUser'] = false;
-$_SESSION['token'] = bin2hex(random_bytes(32));
-?>
--- init.php
+++
@@ -1,7 +0,0 @@
-<?php
-//set up csrf token
-
-
-//set up database
-
-?>
