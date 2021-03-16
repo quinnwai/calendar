23d8bbc88da1b9eed7a7b310d6264e0c5ff25a4d
quinnwaiwong <quinnwai.wong@gmail.com>
2021-03-16 12:36:38 -0500

    clean up

--- add_event.php
+++ add_event.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 ini_set("session.cookie_httponly", 1);
-// header("Content-Type: application/json"); // set the MIME Type to application/json
+header("Content-Type: application/json"); // set the MIME Type to application/json
 
 require 'database.php'; 
 
@@ -11,7 +11,6 @@ $json_str = file_get_contents('php://input');
 $json_obj = json_decode($json_str, true);
 
 // make sure not empty
-// echo "checking empties";
 if ($date = "" || $event_name = "" || $user = "" || $tag = ""){
 	echo json_encode(array(
 		"success" => false,
@@ -27,35 +26,15 @@ require 'get_token.php';
 // Source: https://stackoverflow.com/questions/2350052/how-can-i-get-enum-possible-values-in-a-mysql-database
 
 
-// // do actual work of inserting event into SQL
-// $stmt = $mysqli->prepare("INSERT INTO events (date_time, event_name, username, tag) VALUES (?, ?, ?, ?)");
+// do actual work of inserting event into SQL
 $stmt = $mysqli->prepare("INSERT INTO `events` (`date_time`, `event_name`, `username`, `tag`) VALUES (?,?,?,?)");
-// // $stmt = $mysqli->prepare("INSERT INTO events (date_time, name, username, tag) VALUES
-// // ('2021-03-21 12:00', 'Walk the Dog', 'firstlast', 'sports')");
 
-// access json object (associative array)
-// $date = date("Y-m-d\TH:i:s", strtotime("Sun Mar 14 2021 10:29:34 GMT-0500 (Central Daylight Time)"));
-// $date = date($json_obj['date']); // TODO: make sure works w/ echo $date;
-
+// read in values to bind as params
 $date = DateTime::createFromFormat('Y-m-d\TH:i', (string) $json_obj['date'])->format('Y-m-d H:i:s');
-// $date = DateTime::createFromFormat('Y-m-d\TH:i', "2021-03-21T12:00")->format('Y-m-d H:i:s'); //debug all the way down
-// var_dump($date);
-// echo "\n";
-
 $event_name = (string) $json_obj['event_name'];
-// $event_name = "Walking the Dog";
-// var_dump($event_name);
-// echo "\n";
-
 $user = (string) $json_obj['user'];
-// $user = "firstlast";
-// var_dump($user);
-// echo "\n";
-
 $tag = (string) $json_obj['tag']; //TODO: need to somehow have enum in html or check tag here
-// $tag = "sports";
-// var_dump($tag);
-// echo "\n";
+
 
 
 // //TODO: make sure this works
